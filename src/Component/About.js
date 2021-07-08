04cd1950e13083abbfbcc371001c566dfffcaa2d
Brahmdevsingh <65753877+Brahmdevsingh@users.noreply.github.com>
2021-07-08 11:30:38 +0530

    Update About.js

--- src/Component/About.js
+++ src/Component/About.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import image from '../../public/images/DP.png';
 function About() {
 
     return <div>
         
        <font face="Wildwest" size="20px" color="white">
-           NAME:BRAHM DEV SINGH <br/>
+           NAME:BRAHM DEV SINGH  CHAUHAN<br/>
            ID: 20191CSE0082 <br/>
            SEC.:5CSE02<br/>
            </font>
-           <img src={image}/> 
+           <img src="/assets/images/DP.jpg"><image/> 
     </div>
 
 }
