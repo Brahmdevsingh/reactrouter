f9ba66ac864f8efee3838ccf8b1810192833373b
Brahmdevsingh <65753877+Brahmdevsingh@users.noreply.github.com>
2021-07-08 08:12:27 +0530

    Update About.js

--- src/Component/About.js
+++ src/Component/About.js
@@ -5,7 +5,7 @@ function About() {
     return <div>
         
        <font face="Wildwest" size="20px" color="white">
-           NAME:BRAHM DEV SINGH CHAUHAN<br/>
+           NAME:BRAHM DEV SINGH <br/>
            ID: 20191CSE0082 <br/>
            SEC.:5CSE02<br/>
            </font>
